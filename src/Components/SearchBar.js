f30edf5f4286e89fab48727e6e0c7940654cc5f3
Bailey Mejia <baileymejia@Baileys-MacBook-Pro.local>
2023-06-27 10:18:06 -0700

    Added search functionality to find cities and show the current weather.

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,33 +1,51 @@
 //TODO Create a search bar so the user can search for a city 
 import { useState } from "react";
 import { SearchBarContainer, SearchField, SearchButton } from "./Styled/Search/Styled.SearchBar";
-import useWeatherData from "./WeatherData";
+import { AddCity } from "./AddCity";
+import useWeatherData from "./WeatherData"; 
 
 
 export default function SearchBar() {
     const geoCoding =  process.env.REACT_APP_GEO_CODING_KEY;
     const weatherData = useWeatherData();
+    //Store the cities that are searched into an array list
+    const [cities, setCities] = useState([]);
     const [searchCity, setSearchCity] = useState('');
+    const weatherAPIKey = process.env.REACT_APP_ONE_CALL;
+
+    
 
 
     //Handle the city data search
     const handleSearch = () => {
-        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=${geoCoding}`).then(response => response.json())
-        .then(data => console.log(data[0].name))
-        .catch(error => {
-            console.error('An error has occured while searching', error);
-        });
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&appid=${geoCoding}`)
+        .then(response => response.json())
+        .then(data => {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            const cityName = data[0].name;
+
+            fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherAPIKey}`)
+            .then(response => response.json())
+            .then(weatherData => {
+                setCities(prevCities => [...prevCities, { name: cityName, weather: weatherData }])
+            })
+        })
+        .catch(error => console.error('An error has occured cant find city.', error));
     };
 
     const handleInputChange = (e) => {
         setSearchCity(e.target.value);
     };
  
-console.log(geoCoding);
+
     return (
+        <>
         <SearchBarContainer>
             <SearchField  type="text" value={searchCity} onChange={handleInputChange} placeholder="Find a City..."></SearchField>
             <SearchButton onClick={handleSearch}>Search</SearchButton>
         </SearchBarContainer>
+        {cities.map((city, index) => <AddCity key={index} city={city}  />)}
+        </>
     );
 }
